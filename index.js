3c2d74afd5cf6f37d74bb223127c7c4783e7b3d3
Leo <aammlj2002@gmail.com>
2022-02-10 11:19:43 +0630

    created dedicated routes file

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const app = express();
-app.get("/api/people", function (req, res) {
-    const data = [
-        { name: "Bobo", age: 22 },
-        { name: "Nini", age: 23 },
-    ];
-    return res.status(200).json(data);
-});
+const routes = require("./routes");
+app.use("/api", routes);
 app.listen(8000, function () {
     console.log("Server running at port 8000...");
 });
